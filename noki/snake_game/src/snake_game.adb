6c124128c1ef83a40a23967f73d483f14bc7e71c
trr5283 <trr5283@psu.edu>
2025-11-04 21:44:18 -0500

    Lesson 3 progress

--- noki/snake_game/src/snake_game.adb
+++ noki/snake_game/src/snake_game.adb
@@ -1,6 +1,28 @@
 with Noki;
 
 procedure Snake_Game is
+
+   type Game_State_T is (Welcome, Play, Game_Over, Undefined);
+   Game_State : Game_State_T := Welcome;
+
 begin
-   Noki.Log ("Welcome to my Snake Game!");
+
+   loop
+      Noki.Log (Noki.Clear_Screen);
+      case Game_State is
+         when Welcome =>
+            Noki.Log ("Welcome to Snakotron!");
+            Game_State := Play;
+         when Play =>
+            Noki.Log ("We are playing.");
+            Game_State := Game_Over;
+         when Game_Over =>
+            Noki.Log("Game Over!");
+            Game_State := Undefined;
+         when others =>
+            Noki.Log("Press Ctrl+C to abort program!");
+         end case;
+      delay 1.0;
+   end loop;
+
 end Snake_Game;
--- noki/src/noki.ads
+++ noki/src/noki.ads
@@ -1,5 +1,9 @@
 package Noki is
 
+CSI : constant String := Character'Val (16#1B#) & '[';
+
+function Clear_Screen return String is (CSI & "2J" & CSI & "H");
+
 procedure Log(S : String);
 
 end Noki;
